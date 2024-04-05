58c87cf4c046d1228805eaf6412aa1126a677bc4
Linkerin <130695447+Linkerin@users.noreply.github.com>
2024-04-05 23:04:23 +0300

    feat: updated media preload to load 2 words in advance

--- app/_hooks/useMediaLoad.ts
+++ app/_hooks/useMediaLoad.ts
@@ -5,14 +5,22 @@ import { SUPABASE_STORAGE_URL } from '../_lib/constants';
 function useMediaLoad(current: number, words: any[]) {
   useEffect(() => {
     if (typeof window === 'undefined') return;
+    if (current >= words.length - 2) return;
 
-    if (current < words.length - 1) {
-      const i = current + 1;
+    const load = (i: number) => {
       const img = new Image();
       img.src = `${SUPABASE_STORAGE_URL}/images/480/${words[i].img_name}.webp`;
       const audio = new Audio();
       audio.src = `${SUPABASE_STORAGE_URL}/audio_ro/${words[i].audio_name}.mp3`;
+
+      return;
+    };
+
+    if (current === 0) {
+      load(current + 1);
     }
+
+    load(current + 2);
   }, [current, words]);
 
   return;
