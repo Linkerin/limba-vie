3d835260088d55e92a6fff5c496ee186368170a1
Linkerin <130695447+Linkerin@users.noreply.github.com>
2024-04-07 12:13:29 +0300

    refactor: moved sets loading from SetsList component into the main page component

--- app/_components/SetsList/SetsList.tsx
+++ app/_components/SetsList/SetsList.tsx
@@ -1,26 +1,8 @@
-import { cache } from 'react';
-
 import SetItem from '../SetItem/SetItem';
-import supabase from '@/app/_lib/supabase';
 
 import styles from './SetsList.module.css';
 
-const getSets = cache(async () => {
-  try {
-    const { data, error } = await supabase
-      .from('sets')
-      .select('id, set, emoji, words(count)');
-    if (error) throw error;
-
-    return data;
-  } catch (err) {
-    throw err;
-  }
-});
-
-async function SetsList() {
-  const sets = await getSets();
-
+async function SetsList({ sets }: { sets: any[] }) {
   return (
     <ul className={styles.list} role="list">
       {sets.map(set => {
@@ -31,5 +13,3 @@ async function SetsList() {
 }
 
 export default SetsList;
-
-export const revalidate = 3600;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+import { cache } from 'react';
+
 import SetsList from './_components/SetsList/SetsList';
+import supabase from '@/app/_lib/supabase';
+
+const getSets = cache(async () => {
+  try {
+    const { data, error } = await supabase
+      .from('sets')
+      .select('id, set, emoji, words(count)');
+    if (error) throw error;
+
+    return data;
+  } catch (err) {
+    throw err;
+  }
+});
 
 async function Home() {
-  return <SetsList />;
+  const sets = await getSets();
+
+  return <SetsList sets={sets} />;
 }
 
 export default Home;
+
+export const revalidate = 3600;
