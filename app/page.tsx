290306071df02293357076dff3477c1a6592ee71
Linkerin <130695447+Linkerin@users.noreply.github.com>
2024-04-09 10:59:38 +0300

    feat: sets sorting based on a linked list

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,35 @@ const getSets = cache(async () => {
   try {
     const { data, error } = await supabase
       .from('sets')
-      .select('id, set, emoji, words(count)');
+      .select('id, set, emoji, head, next_set_id, words(count)');
     if (error) throw error;
 
-    return data;
+    const sortedSets: any[] = [];
+    const sets = {
+      sets: data,
+      [Symbol.iterator]() {
+        let set = this.sets.find(set => set.head);
+
+        return {
+          next: () => {
+            if (set) {
+              const currSet = set;
+              set = this.sets.find(setElem => setElem.id === set?.next_set_id);
+
+              return { value: currSet, done: false };
+            } else {
+              return { value: undefined, done: true };
+            }
+          }
+        };
+      }
+    };
+
+    for (const set of sets) {
+      sortedSets.push(set);
+    }
+
+    return sortedSets;
   } catch (err) {
     throw err;
   }
