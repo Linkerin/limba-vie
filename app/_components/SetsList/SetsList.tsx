243ace78e4c9f2a6b1bbe7d380a7ef0991a779a2
Linkerin <130695447+Linkerin@users.noreply.github.com>
2024-04-12 11:04:32 +0300

    feat: unit separation for sets

--- app/_components/SetsList/SetsList.tsx
+++ app/_components/SetsList/SetsList.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import dynamic from 'next/dynamic';
 
 import styles from './SetsList.module.css';
@@ -5,13 +6,36 @@ import styles from './SetsList.module.css';
 const SetItem = dynamic(() => import('../SetItem/SetItem'), { ssr: false });
 
 async function SetsList({ sets }: { sets: any[] }) {
-  return (
-    <ul className={styles.list} role="list">
-      {sets.map(set => {
-        return <SetItem key={set.id} set={set} />;
-      })}
-    </ul>
+  const units = Array.from(new Set(sets.map(set => set.unit))).sort(
+    (a, b) => a - b
   );
+
+  return (
+    <>
+      {units.map(unit => {
+        return (
+          <Fragment key={unit}>
+            <h2 className={styles.h2}>Unit {unit}</h2>
+            <ul className={styles.list} role="list">
+              {sets
+                .filter(set => set.unit === unit)
+                .map(set => {
+                  return <SetItem key={set.id} set={set} />;
+                })}
+            </ul>
+          </Fragment>
+        );
+      })}
+    </>
+  );
+
+  // return (
+  //   <ul className={styles.list} role="list">
+  //     {sets.map(set => {
+  //       return <SetItem key={set.id} set={set} />;
+  //     })}
+  //   </ul>
+  // );
 }
 
 export default SetsList;
