64c3645e21b69fe292e6e9c4185fb57cde73c3c6
Linkerin <130695447+Linkerin@users.noreply.github.com>
2024-04-16 12:59:38 +0300

    refactor: simplified order logic for sets list fetching

--- app/page.tsx
+++ app/page.tsx
@@ -7,35 +7,13 @@ const getSets = cache(async () => {
   try {
     const { data, error } = await supabase
       .from('sets')
-      .select('id, set, emoji, head, next_set_id, unit, words(count)');
+      .select('id, set, emoji, head, next_set_id, unit, words(count)')
+      .order('unit')
+      .order('head', { ascending: false })
+      .order('next_set_id');
     if (error) throw error;
 
-    const sortedSets: any[] = [];
-    const sets = {
-      sets: data,
-      [Symbol.iterator]() {
-        let set = this.sets.find(set => set.head);
-
-        return {
-          next: () => {
-            if (set) {
-              const currSet = set;
-              set = this.sets.find(setElem => setElem.id === set?.next_set_id);
-
-              return { value: currSet, done: false };
-            } else {
-              return { value: undefined, done: true };
-            }
-          }
-        };
-      }
-    };
-
-    for (const set of sets) {
-      sortedSets.push(set);
-    }
-
-    return sortedSets;
+    return data;
   } catch (err) {
     throw err;
   }
