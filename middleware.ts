bd53b05d56e9b66b87f4767180a19acab3a2cd14
Linkerin <130695447+Linkerin@users.noreply.github.com>
2024-04-17 13:58:15 +0300

    fix: potential fix for netlify middleware usage v2

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,11 @@
-// import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
   if (
-    request.url.includes('localhost') &&
+    request.url.includes('netlify') &&
     process.env.NETLIFY_REDIRECT === 'TRUE'
   ) {
     return Response.redirect(new URL('/', 'https://limba.vercel.app'));
-    // return NextResponse.redirect(new URL('/', 'https://limba.vercel.app'));
   }
 }
 
