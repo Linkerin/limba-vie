343457776d0b464d8311b3cacc493c3e80fc5117
Linkerin <130695447+Linkerin@users.noreply.github.com>
2024-04-21 19:43:56 +0300

    refactor: audio status was moved into separate context provider

--- app/_lib/constants.ts
+++ app/_lib/constants.ts
@@ -3,3 +3,7 @@ export const CLOUDINARY_IMG_URL = `${process.env.NEXT_PUBLIC_CLOUDINARY_URL}/ima
 export const SUPABASE_STORAGE_URL = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public`;
 
 export const REPEAT_WORDS_CTY = 20;
+
+export const LOCAL_STORAGE_KEYS = {
+  sound: 'lvAudioAutoplay'
+};
