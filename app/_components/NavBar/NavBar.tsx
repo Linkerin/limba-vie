0f96cceb5c17bfd82b899aaf0c7804212a4bb9f4
Linkerin <130695447+Linkerin@users.noreply.github.com>
2024-04-25 23:40:37 +0300

    refactor: navbar repeat link generation was moved into a separate hook

--- app/_components/NavBar/NavBar.tsx
+++ app/_components/NavBar/NavBar.tsx
@@ -4,71 +4,16 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { IconBarbell, IconBook2, IconHome } from '@tabler/icons-react';
 
-import { REPEAT_WORDS_CTY } from '@/app/_lib/constants';
-import { shuffleArr } from '@/app/_lib/utils';
 import ssrLocalStorage from '@/app/_services/SsrLocalStorage';
+import useShowRepeatBtn from '@/app/_hooks/useShowRepeatBtn';
 
 import styles from './NavBar.module.css';
 import classNames from 'classnames';
 
-function showTrainButton() {
-  const result = {
-    show: false,
-    completedSets: [] as number[],
-    wordsForRepeat: [] as string[]
-  };
-
-  const completedSetsStr = ssrLocalStorage.getItem('lvCompletedSets');
-  const repeatWordsStr = ssrLocalStorage.getItem('lvRepeatWords');
-  if (!completedSetsStr && !repeatWordsStr) return result;
-
-  let setsExist = false;
-  let repeatWordsExist = false;
-
-  if (completedSetsStr) {
-    const completedSets = JSON.parse(completedSetsStr);
-
-    if (completedSets.length === 0) return result;
-
-    setsExist = true;
-    result.completedSets = completedSets;
-  }
-
-  if (repeatWordsStr) {
-    const repeatWords = JSON.parse(repeatWordsStr);
-    const wordIds = Object.keys(repeatWords);
-
-    if (wordIds.length > 0) {
-      repeatWordsExist = true;
-      result.wordsForRepeat = wordIds;
-    }
-  }
-
-  if (setsExist || repeatWordsExist) {
-    result.show = true;
-
-    return result;
-  }
-
-  return result;
-}
-
 function NavBar() {
   const pathname = usePathname();
-  const { show, completedSets, wordsForRepeat } = showTrainButton();
 
-  const setParamArr = shuffleArr(completedSets)
-    .slice(0, 5)
-    .map(val => ['set', `${val}`]);
-  const repeatParamArr = shuffleArr(wordsForRepeat)
-    .slice(0, REPEAT_WORDS_CTY)
-    .map(val => ['r', `${val}`]);
-
-  const params = new URLSearchParams(
-    [...repeatParamArr, ...setParamArr].slice(0, REPEAT_WORDS_CTY)
-  );
-  const url = new URL('/set/lvrepeat', process.env.NEXT_PUBLIC_BASE_URL);
-  url.search = params.toString();
+  const { show, url } = useShowRepeatBtn();
 
   return !pathname.match(/\/set\/?.*/) ? (
     <footer className={styles.footer}>
---
+++ app/_hooks/useShowRepeatBtn.ts
@@ -0,0 +1,68 @@
+'use client';
+
+import { REPEAT_WORDS_CTY } from '@/app/_lib/constants';
+import { shuffleArr } from '../_lib/utils';
+import ssrLocalStorage from '../_services/SsrLocalStorage';
+
+function getWordForRepeat() {
+  try {
+    const repeatWordsStr = ssrLocalStorage.getItem('lvRepeatWords');
+
+    if (repeatWordsStr) {
+      const repeatWords = JSON.parse(repeatWordsStr);
+      const wordIds = Object.keys(repeatWords);
+
+      if (wordIds.length > 0) {
+        return wordIds;
+      }
+    }
+
+    return [];
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+}
+
+function getCompletedSets() {
+  try {
+    const completedSetsStr = ssrLocalStorage.getItem('lvCompletedSets');
+
+    if (completedSetsStr) {
+      const completedSets = JSON.parse(completedSetsStr);
+      if (completedSets.length > 0) return completedSets as number[];
+    }
+
+    return [];
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+}
+
+function useShowRepeatBtn() {
+  let show = false;
+  const completedSets = getCompletedSets();
+  const wordsForRepeat = getWordForRepeat();
+
+  if (completedSets.length > 0 || wordsForRepeat.length > 0) {
+    show = true;
+  }
+
+  const setParamArr = shuffleArr(completedSets)
+    .slice(0, 5)
+    .map(val => ['set', `${val}`]);
+  const repeatParamArr = shuffleArr(wordsForRepeat)
+    .slice(0, REPEAT_WORDS_CTY)
+    .map(val => ['r', `${val}`]);
+
+  const params = new URLSearchParams(
+    [...repeatParamArr, ...setParamArr].slice(0, REPEAT_WORDS_CTY)
+  );
+  const url = new URL('/set/lvrepeat', process.env.NEXT_PUBLIC_BASE_URL);
+  url.search = params.toString();
+
+  return { show, url };
+}
+
+export default useShowRepeatBtn;
