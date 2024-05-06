f3f44c4221532c2d6138db0fc832bf35dd48408e
Linkerin <130695447+Linkerin@users.noreply.github.com>
2024-05-06 11:56:52 +0300

    feat: vibration on finished screen

--- app/_components/WordScreen/Finished/FinishedSound.tsx
+++ app/_components/WordScreen/Finished/FinishedSound.tsx
@@ -1,9 +1,22 @@
 'use client';
 
+import { useEffect } from 'react';
+
 import { useIsSoundAllowed } from '@/app/_hooks/useSoundMode';
 
 function FinishedSound() {
   const isAllowed = useIsSoundAllowed();
+  useEffect(() => {
+    if (typeof navigator === 'undefined') return;
+
+    const timeoutId = setTimeout(() => {
+      navigator.vibrate(300);
+    }, 300);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
 
   return isAllowed ? (
     <audio src="/sounds/tadam.aac" preload="auto" autoPlay />
