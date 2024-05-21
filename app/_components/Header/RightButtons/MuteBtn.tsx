ec82e41bd04d41bfa4589d77965193c4117fb219
Linkerin <130695447+Linkerin@users.noreply.github.com>
2024-05-21 11:39:47 +0300

    test: pop sound for the mute btn click

--- app/_components/Header/RightButtons/MuteBtn.tsx
+++ app/_components/Header/RightButtons/MuteBtn.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 import { IconVolume, IconVolumeOff } from '@tabler/icons-react';
 
 import Button from '../../_ui/Button/Button';
@@ -11,25 +11,38 @@ import styles from './RightButtons.module.css';
 function MuteBtn() {
   const { isSoundAllowed, toggleSound } = useSoundMode();
 
+  const popSound = useMemo(() => new Audio('/sounds/pop.aac'), []);
+
   const clickHandler: React.MouseEventHandler = useCallback(
-    async e => {
+    e => {
       e.preventDefault();
+      if (!isSoundAllowed) {
+        popSound.currentTime = 0;
+        popSound.play();
+      } else {
+        popSound.pause();
+      }
+
       toggleSound();
     },
-    [toggleSound]
+    [toggleSound, isSoundAllowed, popSound]
   );
 
   return (
-    <Button
-      aria-label={`Turn ${isSoundAllowed ? 'off' : 'on'} word sounds autoplay`}
-      className={styles.btn}
-      onClick={clickHandler}
-      title={`Turn ${isSoundAllowed ? 'off' : 'on'} word sounds autoplay`}
-      fadeAnimation
-      vibrate={isSoundAllowed ? false : true}
-    >
-      {isSoundAllowed ? <IconVolume /> : <IconVolumeOff />}
-    </Button>
+    <>
+      <Button
+        aria-label={`Turn ${
+          isSoundAllowed ? 'off' : 'on'
+        } word sounds autoplay`}
+        className={styles.btn}
+        onClick={clickHandler}
+        title={`Turn ${isSoundAllowed ? 'off' : 'on'} word sounds autoplay`}
+        fadeAnimation
+        vibrate={isSoundAllowed ? false : true}
+      >
+        {isSoundAllowed ? <IconVolume /> : <IconVolumeOff />}
+      </Button>
+    </>
   );
 }
 
