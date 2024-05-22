a9c551c4a26d04fde07cd07c8ef0a083f667cbdb
Linkerin <130695447+Linkerin@users.noreply.github.com>
2024-05-22 22:16:00 +0300

    feat: removed sound on MuteBtn click

--- app/_components/Header/RightButtons/MuteBtn.tsx
+++ app/_components/Header/RightButtons/MuteBtn.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useCallback, useRef } from 'react';
+import { useCallback } from 'react';
 import { IconVolume, IconVolumeOff } from '@tabler/icons-react';
 
 import Button from '../../_ui/Button/Button';
@@ -11,44 +11,25 @@ import styles from './RightButtons.module.css';
 function MuteBtn() {
   const { isSoundAllowed, toggleSound } = useSoundMode();
 
-  const audioRef = useRef<HTMLAudioElement>(null);
-
   const clickHandler: React.MouseEventHandler = useCallback(
     e => {
       e.preventDefault();
       toggleSound();
-
-      if (!audioRef.current) return;
-
-      if (!isSoundAllowed) {
-        audioRef.current.currentTime = 0;
-        audioRef.current.play();
-      } else {
-        audioRef.current.pause();
-      }
     },
-    [toggleSound, isSoundAllowed]
+    [toggleSound]
   );
 
   return (
-    <>
-      <Button
-        aria-label={`Turn ${
-          isSoundAllowed ? 'off' : 'on'
-        } word sounds autoplay`}
-        className={styles.btn}
-        onClick={clickHandler}
-        title={`Turn ${isSoundAllowed ? 'off' : 'on'} word sounds autoplay`}
-        fadeAnimation
-        vibrate={isSoundAllowed ? false : true}
-      >
-        {isSoundAllowed ? <IconVolume /> : <IconVolumeOff />}
-      </Button>
-      <audio ref={audioRef} preload="auto">
-        <source src="/sounds/pop.aac" type="audio/mp4" />
-        <source src="/sounds/pop.mp3" type="audio/mpeg" />
-      </audio>
-    </>
+    <Button
+      aria-label={`Turn ${isSoundAllowed ? 'off' : 'on'} word sounds autoplay`}
+      className={styles.btn}
+      onClick={clickHandler}
+      title={`Turn ${isSoundAllowed ? 'off' : 'on'} word sounds autoplay`}
+      fadeAnimation
+      vibrate={isSoundAllowed ? false : true}
+    >
+      {isSoundAllowed ? <IconVolume /> : <IconVolumeOff />}
+    </Button>
   );
 }
 
