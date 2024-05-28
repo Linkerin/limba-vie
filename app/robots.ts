883b44988447858a9522c5ec15dc5aa704ac93b7
Linkerin <130695447+Linkerin@users.noreply.github.com>
2024-05-28 12:53:55 +0300

    feat: added robots.txt

---
+++ app/robots.ts
@@ -0,0 +1,19 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: [
+        '/api/',
+        '/set/',
+        '/404/',
+        '/500/',
+        '/about/legal/terms-of-service',
+        '/about/legal/privacy-policy'
+      ]
+    },
+    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`
+  };
+}
