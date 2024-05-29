ff08793e2dc98b40b16fef50c9910736ea9764f8
Linkerin <130695447+Linkerin@users.noreply.github.com>
2024-05-29 21:31:53 +0300

    refactor: changed folder structure for layout and error pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,21 +2,18 @@ import { Alata } from 'next/font/google';
 import dynamic from 'next/dynamic';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
-import Header from '../_components/Header/Header';
-import SoundProvider from '../_contexts/SoundProvider';
-import WebVitals from '../_components/WebVitals';
+import Header from './_components/Header/Header';
+import SoundProvider from './_contexts/SoundProvider';
+import WebVitals from './_components/WebVitals';
 
 import './globals.css';
 
 export { metadata } from './metadata';
 
-const AnonymousSignIn = dynamic(
-  () => import('../_components/AnonymousSignIn'),
-  {
-    ssr: false
-  }
-);
-const NavBar = dynamic(() => import('../_components/NavBar/NavBar'), {
+const AnonymousSignIn = dynamic(() => import('./_components/AnonymousSignIn'), {
+  ssr: false
+});
+const NavBar = dynamic(() => import('./_components/NavBar/NavBar'), {
   ssr: false
 });
 
