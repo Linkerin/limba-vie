989ae97da1669cf5db387246404d4245987ff3cc
Linkerin <130695447+Linkerin@users.noreply.github.com>
2024-08-21 17:26:12 +0300

    feat: ✨ Added a link to dexonline.ro inside a word page

--- app/_components/Pages/WordPage/WordPage.tsx
+++ app/_components/Pages/WordPage/WordPage.tsx
@@ -1,5 +1,7 @@
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import { css } from '@/styled-system/css';
+import { IconExternalLink } from '@tabler/icons-react';
 
 import AudioBtn from '../../_ui/AudioBtn/AudioBtn';
 import Sentence from '../../Word/Sentence/Sentence';
@@ -17,7 +19,23 @@ const sectionStyles = css({
 });
 
 const sentenceStyles = css.raw({
-  marginBlock: 'token(spacing.6, 1.5rem) auto'
+  marginBlock: 'token(spacing.6, 1.5rem) token(spacing.4, 1rem)'
+});
+
+const dexLinkStyles = css({
+  fontSize: 'xs',
+  marginBlockEnd: 'auto',
+  width: '90%',
+
+  '& > svg': {
+    display: 'inline-block',
+    marginInlineStart: '0.25em',
+    verticalAlign: '-11.5%'
+  },
+
+  '& > span': {
+    fontWeight: 'bold'
+  }
 });
 
 interface WordPageProps {
@@ -29,27 +47,36 @@ function WordPage({ word }: WordPageProps) {
 
   return (
     <section className={sectionStyles}>
-      <>
-        <WordImg
-          wordEn={word.en}
-          gender={word.gender_ro}
-          imgName={word.img_name}
-        />
-        <Word word={word.ro} plural={word.plural} gender={word.gender_ro}>
-          {word.audio_name?.length && (
-            <AudioBtn
-              audioName={word.audio_name}
-              word={word.ro}
-              autoplay={false}
-            />
-          )}
-        </Word>
-        <Sentence
-          css={sentenceStyles}
-          ro={word.example_ro}
-          en={word.example_en}
-        />
-      </>
+      <WordImg
+        wordEn={word.en}
+        gender={word.gender_ro}
+        imgName={word.img_name}
+      />
+      <Word word={word.ro} plural={word.plural} gender={word.gender_ro}>
+        {word.audio_name?.length && (
+          <AudioBtn
+            audioName={word.audio_name}
+            word={word.ro}
+            autoplay={false}
+          />
+        )}
+      </Word>
+      <Sentence
+        css={sentenceStyles}
+        ro={word.example_ro}
+        en={word.example_en}
+      />
+      <Link
+        className={dexLinkStyles}
+        aria-label={`External link to '${word.ro}' word page on DEXOnline.ro`}
+        href={`https://dexonline.ro/definitie/${encodeURIComponent(word.ro)}`}
+        rel="noopener external"
+        target="_blank"
+        prefetch={false}
+      >
+        Check the word at <span>dexonline.ro</span>
+        <IconExternalLink />
+      </Link>
     </section>
   );
 }
