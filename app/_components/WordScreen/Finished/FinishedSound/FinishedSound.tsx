60a86f9cf94756529e12310a31ed00c2d0b71b63
Linkerin <130695447+Linkerin@users.noreply.github.com>
2024-08-24 18:23:07 +0300

    refactor: 🚸 Turned of vibration of a Finished screen when sounds are now allowed

--- app/_components/WordScreen/Finished/FinishedSound/FinishedSound.tsx
+++ app/_components/WordScreen/Finished/FinishedSound/FinishedSound.tsx
@@ -6,8 +6,9 @@ import { useIsSoundAllowed } from '@/app/_hooks/useSoundMode';
 
 function FinishedSound() {
   const isAllowed = useIsSoundAllowed();
+
   useEffect(() => {
-    if (typeof navigator?.vibrate !== 'function') return;
+    if (typeof navigator?.vibrate !== 'function' || !isAllowed) return;
 
     const timeoutId = setTimeout(() => {
       navigator.vibrate([300, 200, 500]);
@@ -16,7 +17,7 @@ function FinishedSound() {
     return () => {
       clearTimeout(timeoutId);
     };
-  }, []);
+  }, [isAllowed]);
 
   return isAllowed ? (
     <audio src="/sounds/tadam.aac" preload="auto" autoPlay />
