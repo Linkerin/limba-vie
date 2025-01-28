10f0c98f0da46ec0848e4c322f72ab939ef02527
Linkerin <130695447+Linkerin@users.noreply.github.com>
2025-01-28 23:11:34 +0300

    refactor: 💄 Fixed CheckInputModal width for desktop layout

--- app/_components/SetScreen/CheckInputModal/CheckInputModal.styles.ts
+++ app/_components/SetScreen/CheckInputModal/CheckInputModal.styles.ts
@@ -4,10 +4,13 @@ export const containerStyles = css({
   animation: 'move-in 0.5s linear',
   position: 'fixed',
   bottom: '0rem',
+  left: '50%',
   borderRadius: 'token(spacing.2, 0.5rem) token(spacing.2, 0.5rem) 0 0',
   fontSize: 'xl',
+  maxWidth: 'content',
   padding:
     'token(spacing.6, 1.5rem) token(spacing.4, 1rem) token(spacing.8, 2rem)',
+  transform: 'translateX(-50%)',
   width: '100%',
   zIndex: 10,
 
